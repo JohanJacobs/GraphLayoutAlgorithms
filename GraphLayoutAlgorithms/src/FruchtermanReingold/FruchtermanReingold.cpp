bd0826461d04c1836658f73a5bdce8fddf5f046f
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2021-10-30 23:31:37 +1300

    refactor: broke step function out into smaller functions.

--- GraphLayoutAlgorithms/src/FruchtermanReingold/FruchtermanReingold.cpp
+++ GraphLayoutAlgorithms/src/FruchtermanReingold/FruchtermanReingold.cpp
@@ -23,94 +23,28 @@ namespace Layouts
 
 	void FruchtermanReingold::Step(uint32_t iterations)
 	{		
-		std::unordered_map<std::string, Vector2> displacement;
+		m_Displacement.clear();
 
-		std::size_t graph_vertex_count = m_Graph->GetVertices().size();
-		float area = m_Width * m_Height;
-		float k = std::sqrt(area / graph_vertex_count);
-		float t = 10.0f * m_Graph->GetVertices().size(); // set initial temperature
-		
-		float half_width = m_Width / 2.0f;
-		float half_height = m_Height / 2.0f;
+		FructermanReingoldProperties p(
+			m_Graph->VertexCount(),
+			m_Width * m_Height,
+			(10.0f * m_Graph->VertexCount()),
+			m_Width / 2.0f,
+			m_Height / 2.0f,
+			std::sqrt( (m_Width * m_Height)/ m_Graph->VertexCount())	
+		);
 
 		for (uint32_t iteration=0; iteration < iterations; iteration++)
 		{
-			//calculate repulsive forces 
-			for (auto& vertex : m_Graph->GetVertices())
-			{
-				displacement[vertex->GetName()].Set(0.0f, 0.0f);
-	
-				for (auto& other_vertex : m_Graph->GetVertices())
-				{
-					if (other_vertex->GetName() == vertex->GetName())
-						continue;
-					
-					Vector2 from_vertex_position = vertex->GetPosition();
-					Vector2 too_vertex_position = other_vertex->GetPosition();
+						
+			CalculateRepulsiveForces(p.k);
+						
+			CalculateAttractiveForces(p.k);
+						
+			ConstrainDisplacement(p.t);
 
-					// special case, 2 vertices have the same position, perturb the vector with a small amount to push vertices away from each other
-					if ( from_vertex_position == too_vertex_position)
-					{						
-						Vector2 small_random_vector = RandomNumbers::GetVector2();
-						from_vertex_position += small_random_vector;
-					}
+			p.t = UpdateTemprature(p.t);
 
-					auto distance_vector = from_vertex_position - too_vertex_position;
-					float distance_vector_magnitude = distance_vector.Magnitude();
-					
-					auto distance_normalized = Vector2::Normalize(distance_vector);
-					
-					float Fr = ((k*k) / ((distance_vector_magnitude == 0.0f)?1.0f:distance_vector_magnitude));
-
-					auto new_displacement = displacement[vertex->GetName()] + distance_normalized * Fr;
-					displacement[vertex->GetName()] = new_displacement; 
-				}
-			}
-
-			// calculate attractive forces 
-			for ( const auto& [from_vertex_name, too_vertex_name] : m_Graph->GetEdges())
-			{
-				auto& from_vertex_data = GetVertexData(from_vertex_name);
-				auto& too_vertex_data = GetVertexData(too_vertex_name);
-
-				auto distance_vector = from_vertex_data->GetPosition() - too_vertex_data->GetPosition();
-				auto distance_vector_magnitude = distance_vector.Magnitude();
-				auto distnace_vector_normalized = Vector2::Normalize(distance_vector);
-
-				auto fa = (distance_vector_magnitude * distance_vector_magnitude)/k;
-				auto attractive_force = distnace_vector_normalized * fa;
-
-				auto from_vertex_new_displacement = displacement[from_vertex_name] - attractive_force;
-				displacement[from_vertex_name] = from_vertex_new_displacement;
-
-				auto too_vertex_new_displacement = displacement[too_vertex_name] + attractive_force;
-				displacement[too_vertex_name] = too_vertex_new_displacement;
-			}
-
-			// limit maximum displacement to the temperature t and prevent from being outside the frame 
-			for (auto& vertex : m_Graph->GetVertices())
-			{
-				auto displacement_normalized = Vector2::Normalize(displacement[vertex->GetName()]);
-				float displacement_magnitude = displacement[vertex->GetName()].Magnitude();
-
-				auto new_position = vertex->GetPosition() + (displacement_normalized * std::min(displacement_magnitude, t));
-				
-				float new_x = std::min(half_width, std::max(-half_width, new_position.x()));
-				float new_y = std::min(half_height, std::max(-half_height, new_position.y()));
-				
-				vertex->SetPosition(new_x, new_y);
-				
-			}
-
-			//reduce the temperature
-			if (t > 0.01f)
-			{
-				t = t * 0.90f;
-			}
-			else
-			{
-				t = 0.01f;
-			}
 		}		
 	}
 
@@ -121,6 +55,97 @@ namespace Layouts
 		assert(result != std::end(m_Graph->GetVertices()));
 
 		return *result; //de-reference the pointer.
+	}
+
+	void FruchtermanReingold::CalculateRepulsiveForces( float k)
+	{
+		for (auto& vertex : m_Graph->GetVertices())
+		{
+			m_Displacement[vertex->GetName()].Set(0.0f, 0.0f);
+
+			for (auto& other_vertex : m_Graph->GetVertices())
+			{
+				if (other_vertex->GetName() == vertex->GetName())
+					continue;
+
+
+				Vector2 from_vertex_position = vertex->GetPosition();
+				Vector2 too_vertex_position = other_vertex->GetPosition();
+
+				// special case, 2 vertices have the same position, perturb the vector with a small amount to push vertices away from each other
+				if (from_vertex_position == too_vertex_position)
+				{
+					Vector2 small_random_vector = RandomNumbers::GetVector2();
+					from_vertex_position += small_random_vector;
+				}
+
+				auto distance_vector = from_vertex_position - too_vertex_position;
+				float distance_vector_magnitude = distance_vector.Magnitude();
+
+				auto distance_normalized = Vector2::Normalize(distance_vector);
+
+				float Fr = ((k * k) / ((distance_vector_magnitude == 0.0f) ? 1.0f : distance_vector_magnitude));
+
+				auto new_displacement = m_Displacement[vertex->GetName()] + (distance_normalized * Fr);
+				m_Displacement[vertex->GetName()] = new_displacement;
+			}
+		}
+	}
+
+	void FruchtermanReingold::CalculateAttractiveForces(float k)
+	{
+		for (const auto& [from_vertex_name, too_vertex_name] : m_Graph->GetEdges())
+		{
+			auto& from_vertex_data = GetVertexData(from_vertex_name);
+			auto& too_vertex_data = GetVertexData(too_vertex_name);
+
+			auto distance_vector = from_vertex_data->GetPosition() - too_vertex_data->GetPosition();
+			auto distance_vector_magnitude = distance_vector.Magnitude();
+			auto distnace_vector_normalized = Vector2::Normalize(distance_vector);
+
+			auto fa = (distance_vector_magnitude * distance_vector_magnitude) / k;
+			auto attractive_force = distnace_vector_normalized * fa;
+
+			auto from_vertex_new_displacement = m_Displacement[from_vertex_name] - attractive_force;
+			m_Displacement[from_vertex_name] = from_vertex_new_displacement;
+
+			auto too_vertex_new_displacement = m_Displacement[too_vertex_name] + attractive_force;
+			m_Displacement[too_vertex_name] = too_vertex_new_displacement;
+		}
+	}
+
+	void FruchtermanReingold::ConstrainDisplacement(float t)
+	{
+		float half_width = m_Width / 2.0f;
+		float half_height= m_Height/ 2.0f;
+
+		for (auto& vertex : m_Graph->GetVertices())
+		{
+			auto displacement_normalized = Vector2::Normalize(m_Displacement[vertex->GetName()]);
+			float displacement_magnitude = m_Displacement[vertex->GetName()].Magnitude();
+
+			auto new_position = vertex->GetPosition() + (displacement_normalized * std::min(displacement_magnitude, t));
+
+			float new_x = std::min(half_width, std::max(-half_width, new_position.x()));
+			float new_y = std::min(half_height, std::max(-half_height, new_position.y()));
+
+			vertex->SetPosition(new_x, new_y);
+		}
+	}
+
+	float FruchtermanReingold::UpdateTemprature(float t)
+	{
+		//reduce the temperature
+		if (t > 0.01f)
+		{
+			t = t * 0.90f;
+		}
+		else
+		{
+			t = 0.01f;
+		}
+
+		return t;
 	}
 
 }
--- GraphLayoutAlgorithms/src/FruchtermanReingold/FruchtermanReingold.h
+++ GraphLayoutAlgorithms/src/FruchtermanReingold/FruchtermanReingold.h
@@ -29,6 +29,25 @@ namespace Layouts
 		- Straight		
 	*/
 
+	// simple struct to keep track of the properties used.
+	struct FructermanReingoldProperties
+	{
+		// default values will cause no movement on the graph.
+		FructermanReingoldProperties(std::size_t vertex_count = 0, float area = 0.0f, float t = 0.0f,float half_width = 0.0f, float half_height = 0.0f,float k = 0.0f)
+			:vertex_count{ vertex_count }, area{ area }, t{ t }, half_width{ half_width }, half_height{ half_height }, k{k}
+		{
+
+		}
+		~FructermanReingoldProperties() = default;
+
+		std::size_t vertex_count;
+		float area;		// area * width 
+		float k;		// sqrt( |area|)
+		float t;		// temperature
+		float half_width;
+		float half_height;
+	};
+
 	class FruchtermanReingold : public Layouts::ILayout
 	{
 	public:
@@ -40,10 +59,15 @@ namespace Layouts
 	private:
 		const std::shared_ptr<Vertex::IVertex>& GetVertexData(const std::string& vertex_name);
 
+		void CalculateRepulsiveForces(float k);
+		void CalculateAttractiveForces(float k);
+		void ConstrainDisplacement(float t);
+		float UpdateTemprature(float t);
 
 	private:
 		std::shared_ptr<Graphs::IGraph> m_Graph;
 		float m_Width;
 		float m_Height;
+		std::unordered_map<std::string, Vector2> m_Displacement;
 	};
 }
