ed4981ef24fa00470b9389e9551de1e1db84b486
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2021-11-12 13:00:48 +1300

    udpated premake file

--- Sandbox/premake5.lua
+++ Sandbox/premake5.lua
@@ -2,7 +2,7 @@ project "Sandbox"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
-    staticruntime "off"
+    staticruntime "on"
 
     targetdir(TargetDirectory)
     objdir(ObjectDirectory)
